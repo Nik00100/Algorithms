29f4c1205ce2a1e896294deb03c7efa3ec5d7243
Nik <kk@example.com>
2022-12-18 18:59:49 +0300

    add 1.1

---
+++ src/yandex/intersections/Main.java
@@ -0,0 +1,24 @@
+package yandex.intersections;
+
+import java.util.*;
+
+public class Main {
+    static int[] intersection(int[] nums1, int[] nums2) {
+        Set<Integer> numbers = new HashSet<>();
+        for (int n : nums1) { numbers.add(n); }
+        int[] res = new int[numbers.size()];
+        int cursor = 0;
+        for (int n : nums2) {
+            if (numbers.remove(n)) {
+                res[cursor++] = n;
+            }
+        }
+        return Arrays.copyOfRange(res,0,cursor);
+    }
+
+    public static void main(String[] args) {
+        int[] nums1 = {4,9,5};
+        int[] nums2 = {9,4,9,8,4};
+        Arrays.stream(intersection(nums1,nums2)).forEach(i-> System.out.print(i+" "));
+    }
+}
---
+++ src/yandex/rle/Main.java
@@ -0,0 +1,37 @@
+package yandex.rle;
+
+public class Main {
+    public static String getRLE(String input) {
+        StringBuilder result = new StringBuilder();
+        int lengthOfInput = input.length();
+        char lastCharacter = input.charAt(0);
+        int lastCharacterCount = 1;
+
+        // we will go until equal to the length of input and we will do the final flush inside the loop
+        // in this way we will have roughly 90% - 95% performance improvent over nested iteration
+        // but this might take a little more memory as compared to the nested loop iteration
+        for (int i = 1; i <= lengthOfInput; i++) {
+
+            if (i == lengthOfInput) {
+                // we have already completed everything let us append the final value of i - 1 iteration
+                result.append(lastCharacter).append(lastCharacterCount);
+                break;
+            }
+
+            char currentCharacter = input.charAt(i);
+
+            if (lastCharacter == currentCharacter) {
+                lastCharacterCount++;
+            } else {
+                result.append(lastCharacter).append(lastCharacterCount);
+                lastCharacter = currentCharacter;
+                lastCharacterCount = 1;
+            }
+        }
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(getRLE("aaaaaaaaaaaabbbccca"));
+    }
+}
