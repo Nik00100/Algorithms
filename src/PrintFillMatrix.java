1a6dedcf6cf5bca9c224eac60b2d954aa60fe4b4
kirillovnick <kirillovnick@mail.ru>
2023-03-12 11:26:05 +0300

    add v1.54

--- src/PrintFillMatrix.java
+++ src/PrintFillMatrix.java
@@ -1,9 +1,17 @@
+
 import java.util.Arrays;
 import java.util.stream.Collectors;
 
-public class PrintMatrix {
+public class PrintFillMatrix {
+    static void fillMatrix(int[][] matrix, int value) {
+        for (int i = 0; i < matrix.length; i++)
+            Arrays.fill(matrix[i], value);
+    }
+
+
     static void printMatrix(int[][] matrix) {
         Arrays.stream(matrix)
                 .forEach(array-> System.out.println(Arrays.stream(array).boxed().collect(Collectors.toList())));
+        System.out.println("*********************");
     }
 }
--- src/ya_training/algo3_0/task6_06_03/divA/task4/Main.java
+++ src/ya_training/algo3_0/task6_06_03/divA/task4/Main.java
@@ -56,39 +56,38 @@ M (1≤M≤400) — количество роботов. Затем вводят
 
 2*/
 
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
-public class Solution {
-    static int n, k, nr;
-    static int[][][] m = new int[401][401][2];
+public class Main {
+    static int n, k, m;
+    static int[][][] dist = new int[401][401][2];
+    static int[] robots = new int[401];
+    static boolean[] visited = new boolean[401];
     static int[][] l = new int[401][401];
-    static int[] r = new int[401];
     static int[] a = new int[20001];
     static int[] b = new int[20001];
-
-    static boolean[] u = new boolean[401];
-    static int[] o = new int[1000001];
-
     static boolean[][] s = new boolean[401][401];
+    static final int INF = 1000_000;
 
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        PrintWriter out = new PrintWriter(System.out);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("input.txt")));
+        String[] nk = reader.readLine().split(" ");
 
-        n = sc.nextInt();
-        for (int i = 1; i <= 400; i++) {
-            for (int j = 1; j <= 400; j++) {
-                m[i][j][0] = m[i][j][1] = 1000_000;
+        n = Integer.parseInt(nk[0]);
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                dist[i][j][0] = dist[i][j][1] = INF;
             }
         }
-        k = sc.nextInt();
+        k = Integer.parseInt(nk[1]);
         for (int i = 1; i <= n; i++) {
-            m[i][i][0] = 0;
+            dist[i][i][0] = 0;
         }
         for (int i = 1; i <= k; i++) {
-            a[i] = sc.nextInt();
-            b[i] = sc.nextInt();
+            String[] ab = reader.readLine().split(" ");
+            a[i] = Integer.parseInt(ab[0]);
+            b[i] = Integer.parseInt(ab[1]);
             s[a[i]][b[i]] = true;
             s[b[i]][a[i]] = true;
         }
@@ -103,36 +102,36 @@ public class Solution {
             }
         }
 
-        nr = sc.nextInt();
-        for (int i = 1; i <= nr; i++) {
-            r[i] = sc.nextInt();
+        m = Integer.parseInt(reader.readLine());
+        String[] r = reader.readLine().split(" ");
+        for (int i = 1; i <= m; i++) {
+            robots[i] = Integer.parseInt(r[i - 1]);
         }
 
-        for (int i = 1; i <= nr; i++) {
-            bfs(r[i]);
+        for (int i = 1; i <= m; i++) {
+            bfs(robots[i]);
         }
 
-        int an = 1000000;
+        int an = INF;
         for (int i = 1; i <= k; i++) {
             int c = 0;
-            for (int j = 1; j <= nr; j++) {
-                c = Math.max(c, Math.min(Math.min(m[r[j]][a[i]][0], m[r[j]][a[i]][1]),
-                        Math.min(m[r[j]][b[i]][0], m[r[j]][b[i]][1])) + 1);
+            for (int j = 1; j <= m; j++) {
+                c = Math.max(c, Math.min(Math.min(dist[robots[j]][a[i]][0], dist[robots[j]][a[i]][1]),
+                        Math.min(dist[robots[j]][b[i]][0], dist[robots[j]][b[i]][1])) + 1);
             }
             an = Math.min(an, c);
         }
         for (int i = 1; i <= n; i++) {
             int lc = 0;
             int ln = 0;
-            for (int j = 1; j <= nr; j++) {
-                lc = Math.max(lc, m[r[j]][i][0]);
-                ln = Math.max(ln, m[r[j]][i][1]);
+            for (int j = 1; j <= m; j++) {
+                lc = Math.max(lc, dist[robots[j]][i][0]);
+                ln = Math.max(ln, dist[robots[j]][i][1]);
             }
             an = Math.min(an, Math.min(lc, ln));
         }
 
-        if (an < 100000) {
-            //out.printf("%.1f\n", an / 2.0);
+        if (an < INF) {
             double minTimeForAllRobots = (double) an / 2.0;
             double reminder = minTimeForAllRobots % 1;
             if (reminder == 0.0) {
@@ -141,38 +140,36 @@ public class Solution {
                 System.out.println(minTimeForAllRobots);
             }
         } else {
-            out.println("-1");
+            System.out.println("-1");
         }
 
-        sc.close();
-        out.close();
+        reader.close();
     }
+
     static void bfs(int v) {
-        Arrays.fill(u, false);
-        int p1 = 1;
-        int p2 = 1;
-        o[p1] = v;
-        u[v] = true;
-        while (p1 <= p2) {
-            int cv = o[p1++];
-            u[cv] = false;
+        Arrays.fill(visited, false);
+        Queue<Integer> q = new LinkedList<>();
+        q.add(v);
+        visited[v] = true;
+        while (!q.isEmpty()) {
+            int cv = q.poll();
+            visited[cv] = false;
             for (int i = 1; i <= l[cv][0]; i++) {
-                if (m[v][l[cv][i]][0] > m[v][cv][1] + 2) {
-                    m[v][l[cv][i]][0] = m[v][cv][1] + 2;
-                    if (!u[l[cv][i]]) {
-                        u[l[cv][i]] = true;
-                        o[++p2] = l[cv][i];
+                if (dist[v][l[cv][i]][0] > dist[v][cv][1] + 2) {
+                    dist[v][l[cv][i]][0] = dist[v][cv][1] + 2;
+                    if (!visited[l[cv][i]]) {
+                        visited[l[cv][i]] = true;
+                        q.add(l[cv][i]);
                     }
                 }
-                if (m[v][l[cv][i]][1] > m[v][cv][0] + 2) {
-                    m[v][l[cv][i]][1] = m[v][cv][0] + 2;
-                    if (!u[l[cv][i]]) {
-                        u[l[cv][i]] = true;
-                        o[++p2] = l[cv][i];
+                if (dist[v][l[cv][i]][1] > dist[v][cv][0] + 2) {
+                    dist[v][l[cv][i]][1] = dist[v][cv][0] + 2;
+                    if (!visited[l[cv][i]]) {
+                        visited[l[cv][i]] = true;
+                        q.add(l[cv][i]);
                     }
                 }
             }
         }
     }
-
 }
